f745321eaa7f7f5da54dfb6227567bd5637adbb5
gowrihiremath <71113447+gowrihiremath@users.noreply.github.com>
2022-06-20 19:37:29 +0530

    Add files via upload
    RTL and Test Bench codes for a Half Adder

---
+++ half_adder_tb.v
@@ -0,0 +1,25 @@
+module half_adder_tb();
+reg a,b;
+wire sum,carry;
+integer i;
+half_adder UUT(a,b,sum,carry);
+initial 
+   begin
+   a=1'b0;
+   b=1'b0;
+   end
+
+initial
+begin
+   for(i=0; i<4; i=i+1)
+   begin
+   {a,b}=i;
+   #10;
+   end
+end
+
+initial
+$monitor("Input a=%b, b=%b, Output sum=%b, carry=%b", a,b,sum,carry);
+initial #100 $finish;
+endmodule
+
